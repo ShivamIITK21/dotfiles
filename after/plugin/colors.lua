139c62aaa5db5d17fd7d05785afd44038ad08c30
Shivam <sshivam21@iitk.ac.in>
2024-01-14 22:49:51 +0530

    minor change

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -4,9 +4,6 @@ function Colors(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
-	vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
 
 end
 
