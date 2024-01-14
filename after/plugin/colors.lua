731a398ebaf3bdc3753d974943e3d5ed36a058d1
Shivam <sshivam21@iitk.ac.in>
2024-01-14 22:46:06 +0530

    first commit

---
+++ after/plugin/colors.lua
@@ -0,0 +1,13 @@
+function Colors(color)
+	color = color or "catppuccin"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+	vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+
+end
+
+Colors()
