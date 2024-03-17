8bd021b8a1a991c5035716dfedbb255612f43fb7
Shivam <sshivam21@iitk.ac.in>
2024-03-18 00:57:26 +0530

    changed background color

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -4,6 +4,8 @@ function Colors(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalSB", { bg = "none" })
     vim.api.nvim_set_hl(0, 'LineNr', { fg = "white"})
 end
 
